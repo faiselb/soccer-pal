d184645af2050e2b5c3303b4dfe60730fc294e44
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-25 15:49:06 -0400

    profile lookup route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -61,6 +61,24 @@ router.get('/handle/:handle', (req, res) => {
         .catch(err => res.status(404).json(err));
 });
 
+router.get('/user/:user_id', (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.params.user_id })
+        .populate('user', ['name'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                res.status(404).json(errors);
+            }
+
+            res.json(profile);
+        })
+        .catch(err =>
+            res.status(404).json({ profile: 'There is no profile for this user' })
+        );
+});
+
 
 module.exports = router;
 
