933c55810f1bd1f2a93b2db3d981e7598b2cb9d1
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-25 12:26:07 -0400

    createing profile route

---
+++ routes/api/profile.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
