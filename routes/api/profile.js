27410467a61a57bf90cb50ee650563c5cb190e88
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-25 15:01:44 -0400

    create get profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,6 +9,25 @@ const validateProfileInput = require('../../validation/profile');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
+router.get(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const errors = {};
+
+        Profile.findOne({ user: req.user.id })
+            .populate('user', ['name'])
+            .then(profile => {
+                if (!profile) {
+                    errors.noprofile = 'There is no profile for this user';
+                    return res.status(404).json(errors);
+                }
+                res.json(profile);
+            })
+            .catch(err => res.status(404).json(err));
+    }
+);
+
 
 
 
