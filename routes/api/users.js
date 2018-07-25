da373046e0d084ebbec14f2cacb15e50ecf15ecb
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-24 20:57:15 -0400

    import validators

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
 const User = require('../../models/User');
 
 module.exports = router;
