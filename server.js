e4e6b2f524dcc2c901cee42bca8b150f184c6122
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-25 10:57:53 -0400

    In the “Search for Venues” page, the user can view a google map on the side bar. The app is connected to Google Maps, Geolocations and Google Places APIs.

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 require('dotenv').config()
 
 const passport = require('passport');
+
+const users = require('./routes/api/users');
+
+const profile = require('./routes/api/profile');
+const updates = require('./routes/api/updates');
+const places = require('./routes/api/places');
+const meetups = require('./routes/api/meetups');
+
 const app = express();
 
 app.use(express.static(__dirname + '/client/build/'))
@@ -23,6 +31,14 @@ mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+
+  app.use('/api/users', users);
+
+  app.use('/api/profile', profile);
+  app.use('/api/updates', updates);
+  app.use('/api/places', places);
+  app.use('/api/meetups', meetups);
 
 const port = process.env.PORT || 5000;
 
