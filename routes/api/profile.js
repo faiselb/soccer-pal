ba3962dfc8b23a8f2c08f12c87d9c2db7c6960a9
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-25 15:17:50 -0400

    In “create route to get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -28,6 +28,22 @@ router.get(
     }
 );
 
+router.get('/all', (req, res) => {
+    const errors = {};
+  
+    Profile.find()
+      .populate('user', ['name'])
+      .then(profiles => {
+        if (!profiles) {
+          errors.noprofile = 'There are no profiles';
+          return res.status(404).json(errors);
+        }
+  
+        res.json(profiles);
+      })
+      .catch(err => res.status(404).json({ profile: 'There are no profiles' }));
+  });
+
 
 
 
