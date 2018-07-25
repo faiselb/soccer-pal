97d1acf7868dad8ca365d9258d4512e1f05b27b7
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-24 21:30:16 -0400

    Added login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,4 +45,49 @@ router.post('/register', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+  
+    
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+  
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    
+    User.findOne({ email }).then(user => {
+      
+      if (!user) {
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
+      }
+  
+      
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          
+          const payload = { id: user.id, name: user.name}; 
+  
+          
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: 'Bearer ' + token
+              });
+            }
+          );
+        } else {
+          errors.password = 'Password incorrect';
+          return res.status(400).json(errors);
+        }
+      });
+    });
+});
+
 module.exports = router;
