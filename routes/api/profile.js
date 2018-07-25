6d1892cdd9114e2a2a61a8d561065d965871f40c
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-25 12:30:21 -0400

    Import mongoose and passport in profile controller

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+const passport = require('passport');
+
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
