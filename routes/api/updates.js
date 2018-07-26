f57dc0d21f54c937d42c0e184742593b25f33161
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-26 13:10:08 -0400

    added index route to updates

--- routes/api/updates.js
+++ routes/api/updates.js
@@ -3,7 +3,18 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+const Update = require('../../models/Update');
+const Profile = require('../../models/Profile');
 
 const validateUpdateInput = require('../../validation/update');
+
+router.get('/', (req, res) => {
+    Update.find()
+        .sort({ date: -1 })
+        .then(updates => res.json(updates))
+        .catch(err => res.status(404).json({ noupdatesfound: 'No updates found' }));
+});
+
+
 
 module.exports = router;
