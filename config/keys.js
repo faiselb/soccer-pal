8fb8c33ec8b508cdcae16a2315b5a568ab71de3a
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-25 21:29:33 -0400

    added keys for google apis

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
 module.exports = {
-    mongoURI: process.env.MONGODB_URI
+    mongoURI: process.env.MONGODB_URI,
+    secretOrKey: 'secret',
+    PLACES_KEY : 'AIzaSyBG02uCl06PgfJT23CYNs7TsnaCHrn-I8w',
+    GEO_KEY : 'AIzaSyAwJGORCn3CEl-wGnVrMMUpVWbcLkflk6Y'
 };
   
