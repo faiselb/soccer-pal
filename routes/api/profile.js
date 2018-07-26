05688a0f80ecde0a66f3c8f8d9c8906531f8fc66
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-25 20:25:24 -0400

    profile creation route-delete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -79,6 +79,114 @@ router.get('/user/:user_id', (req, res) => {
         );
 });
 
+router.post(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateProfileInput(req.body);
+
+
+        if (!isValid) {
+
+            return res.status(400).json(errors);
+        }
+
+
+        const profileFields = {};
+        profileFields.user = req.user.id;
+        if (req.body.handle) profileFields.handle = req.body.handle;
+        if (req.body.location) profileFields.location = req.body.location;
+        if (req.body.bio) profileFields.bio = req.body.bio;
+        if (typeof req.body.interests !== 'undefined') {
+            profileFields.interests = req.body.interests.split(',');
+        }
+
+        Profile.findOne({ user: req.user.id }).then(profile => {
+            if (profile) {
+
+                Profile.findOneAndUpdate(
+                    { user: req.user.id },
+                    { $set: profileFields },
+                    { new: true }
+                ).then(profile => res.json(profile));
+            } else {
+
+                Profile.findOne({ handle: profileFields.handle }).then(profile => {
+                    if (profile) {
+                        errors.handle = 'That handle already exists';
+                        res.status(400).json(errors);
+                    } else {
+                        new Profile(profileFields).save().then(profile => res.json(profile));
+                    }
+                });
+            }
+        });
+    }
+);
+
+
+router.post(
+    '/createdmeetup',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateCreatedmeetupInput(req.body);
+
+
+        if (!isValid) {
+
+            return res.status(400).json(errors);
+        }
+
+        Profile.findOne({ user: req.user.id }).then(profile => {
+            const newCmeetup = {
+                meetuptitle: req.body.meetuptitle,
+                createdby: req.body.createdby,
+                location: req.body.location,
+                meetupdate: req.body.meetupdate,
+                meetuptime: req.body.meetuptime,
+                description: req.body.description
+            };
+
+
+            profile.createdmeetup.unshift(newCmeetup);
+
+            profile.save().then(profile => res.json(profile));
+        });
+    }
+);
+
+router.delete(
+    '/createdmeetup/:cmeetup_id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Profile.findOne({ user: req.user.id })
+            .then(profile => {
+
+                const removeIndex = profile.createdmeetup
+                    .map(item => item.id)
+                    .indexOf(req.params.cmeetup_id);
+
+
+                profile.createdmeetup.splice(removeIndex, 1);
+
+
+                profile.save().then(profile => res.json(profile));
+            })
+            .catch(err => res.status(404).json(err));
+    }
+);
+
+router.delete(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+            User.findOneAndRemove({ _id: req.user.id }).then(() =>
+                res.json({ success: true })
+            );
+        });
+    }
+);
 
 module.exports = router;
 
