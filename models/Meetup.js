66546a4553043c0fec31fa8aeefaa260ad6ee18e
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-27 11:16:14 -0400

    meetup schema

---
+++ models/Meetup.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const MeetupSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  createdby: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  joinedUsers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  location: {
+    type: String,
+    required: false
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  time: {
+    type: String,
+    required: false
+  },
+  description: {
+    type: String
+  },
+});
+
+module.exports = Meetup = mongoose.model('Meetup', MeetupSchema);
