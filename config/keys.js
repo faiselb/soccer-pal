288e54b61b0a8fd50c1cd35c490b580f1d18ffe3
faisel-burqawi <faisel.burqawi@gmail.com>
2018-07-28 16:51:05 -0400

    put keys in environment variables instead of in code

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongoURI: process.env.MONGODB_URI,
     secretOrKey: 'secret',
-    PLACES_KEY: 'AIzaSyBG02uCl06PgfJT23CYNs7TsnaCHrn-I8w',
-    GEO_KEY: 'AIzaSyAwJGORCn3CEl-wGnVrMMUpVWbcLkflk6Y'
+    PLACES_KEY: process.env.PLACE_KEY,
+    GEO_KEY: process.env.GEO_KEY
 };
